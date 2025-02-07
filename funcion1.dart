328b1816553623610fdca15c89c46272c7863cfe
David Martínez <164534226+David-Martinez-1077@users.noreply.github.com>
2025-02-07 16:47:42 +0000

    Mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,21 @@
+//función que mo devuelve valor
+//función saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('David Yair Martínez Nava');
+}
+
+void main(){
+  // David Yair Martínez Nava, num.control: 22308051281077
+  //llamar a la función saludar
+  print('David Yair Martínez Nava, num.control: 22308051281077');
+  print("Llamando a la función saludar");
+  saludar();
+  print("Llamando a la función escribeNombre");
+  escribeNombre();
+
+  
+}
