a9ca155288e1818a92282811911c8f99857d5206
David Martínez <164534226+David-Martinez-1077@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,16 @@
+//Funciones creadas por el usuario
+void suma(int a, int b){
+  print('La suma de $a + $b es: ${a+b}');
+}
+
+void resta(int a, int b){
+  print('La resta de $a - $b es: ${a-b}');
+}
+
+//función main
+void main(){
+  print('David Yair Martínez Nava, num.control: 22308051281077');
+  print("Llamando a la función suma y resta");
+  suma(5, 3);
+  resta(5, 3);
+}
